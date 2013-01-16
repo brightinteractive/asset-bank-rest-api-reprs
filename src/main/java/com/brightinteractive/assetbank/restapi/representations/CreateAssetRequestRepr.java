87aee6bdc2d137f8a2004acd723c73dcb92fc226
Francis Devereux <francis@bright-interactive.co.uk>
2013-01-16 16:45:31 +0000

    Add new Repr and API version to allow AssetsBaseResource to set access levels on the assets it creates
    Note: AssetsBaseResource is in asset-bank, not this project.
    
    AB-267

---
+++ src/main/java/com/brightinteractive/assetbank/restapi/representations/CreateAssetRequestRepr.java
@@ -0,0 +1,36 @@
+package com.brightinteractive.assetbank.restapi.representations;
+
+/*
+ * Copyright 2013 Bright Interactive, All Rights Reserved.
+ */
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * <p>The data needed to create an asset.</p>
+ *
+ * <p>This is kept to a minimum - the usual process is that an asset is created
+ * and then immediately updated, instead of being created fully populated.</p>
+ *
+ * @author Bright Interactive
+ */
+@XmlRootElement(name="createAsset")
+public class CreateAssetRequestRepr
+{
+	@XmlElementWrapper
+	@XmlElement(name = "accessLevel")
+	public List<AccessLevelRepr> accessLevels;
+
+	public CreateAssetRequestRepr(List<AccessLevelRepr> a_accessLevels)
+	{
+		accessLevels = a_accessLevels;
+	}
+
+	public CreateAssetRequestRepr()
+	{
+	}
+}
--- src/main/java/com/brightinteractive/assetbank/restapi/representations/RootRepr.java
+++ src/main/java/com/brightinteractive/assetbank/restapi/representations/RootRepr.java
@@ -57,10 +57,28 @@ public class RootRepr {
         public URL displayAttributeGroupUrl;
     }
 
+    public static class RootServices_v_1_3
+    {
+        public URL checkoutUrl;
+        public URL editorDependenciesUrl;
+        public URL lightboxesUrl;
+        public URL assetsUrl;
+        public URL usersUrl;
+        public URL userSearchUrl;
+        public URL attributesUrl;
+        public URL categorySearchUrl;
+        public URL accessLevelSearchUrl;
+        public URL signingUrl;
+        public URL assetSearchUrl;
+        public URL publishingActionsUrl;
+        public URL displayAttributeGroupUrl;
+    }
+
     public String api_name;
     public RootServices_v_1_1 api_version_1_1;
     public RootServices_v_1_2 api_version_1_2;
-    public static final String LATEST_API_VERSION = "api_version_1_2";
+    public RootServices_v_1_3 api_version_1_3;
+    public static final String LATEST_API_VERSION = "api_version_1_3";
 
     // just to make JAXB happy
     public RootRepr()
@@ -72,6 +90,7 @@ public class RootRepr {
                     URL a_lightboxesUrl,
                     URL a_lightboxes_v1_1_Url,
                     URL a_assetsUrl,
+                    URL a_assets_v1_2_Url,
                     URL a_usersUrl,
                     URL a_userSearchUrl,
                     URL a_attributesUrl,
@@ -89,7 +108,7 @@ public class RootRepr {
         api_version_1_1.checkoutUrl = a_checkoutUrl;
         api_version_1_1.editorDependenciesUrl = a_editorDependenciesUrl;
         api_version_1_1.lightboxesUrl = a_lightboxes_v1_1_Url;
-        api_version_1_1.assetsUrl = a_assetsUrl;
+        api_version_1_1.assetsUrl = a_assets_v1_2_Url;
         api_version_1_1.signingUrl = a_signingUrl;
 
         //and now the 1.2...
@@ -97,7 +116,7 @@ public class RootRepr {
         api_version_1_2.checkoutUrl = a_checkoutUrl;
         api_version_1_2.editorDependenciesUrl = a_editorDependenciesUrl;
         api_version_1_2.lightboxesUrl = a_lightboxesUrl;
-        api_version_1_2.assetsUrl = a_assetsUrl;
+        api_version_1_2.assetsUrl = a_assets_v1_2_Url;
         api_version_1_2.usersUrl = a_usersUrl;
         api_version_1_2.userSearchUrl = a_userSearchUrl;
         api_version_1_2.attributesUrl = a_attributesUrl;
@@ -107,5 +126,21 @@ public class RootRepr {
         api_version_1_2.assetSearchUrl = a_assetSearchUrl;
         api_version_1_2.publishingActionsUrl = a_publishingActionsUrl;
         api_version_1_2.displayAttributeGroupUrl = a_displayAttributeGroupUrl;
+
+        //and now the 1.3...
+        api_version_1_3 = new RootServices_v_1_3();
+        api_version_1_3.checkoutUrl = a_checkoutUrl;
+        api_version_1_3.editorDependenciesUrl = a_editorDependenciesUrl;
+        api_version_1_3.lightboxesUrl = a_lightboxesUrl;
+        api_version_1_3.assetsUrl = a_assetsUrl;
+        api_version_1_3.usersUrl = a_usersUrl;
+        api_version_1_3.userSearchUrl = a_userSearchUrl;
+        api_version_1_3.attributesUrl = a_attributesUrl;
+        api_version_1_3.categorySearchUrl = a_categorySearchUrl;
+        api_version_1_3.accessLevelSearchUrl = a_accessLevelSearchUrl;
+        api_version_1_3.signingUrl = a_signingUrl;
+        api_version_1_3.assetSearchUrl = a_assetSearchUrl;
+        api_version_1_3.publishingActionsUrl = a_publishingActionsUrl;
+        api_version_1_3.displayAttributeGroupUrl = a_displayAttributeGroupUrl;
     }
 }
