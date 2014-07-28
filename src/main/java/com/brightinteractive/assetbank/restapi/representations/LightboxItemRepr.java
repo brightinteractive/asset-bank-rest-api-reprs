28685b2fe1969e3b45cdda502f96511b6735d23e
Giorgos Georgiou <george@bright-interactive.co.uk>
2014-07-28 10:48:56 +0100

    Add instance and content urls as separate entries for the Lightbox Repr
    AB-1789

--- src/main/java/com/brightinteractive/assetbank/restapi/representations/LightboxItemRepr.java
+++ src/main/java/com/brightinteractive/assetbank/restapi/representations/LightboxItemRepr.java
@@ -23,6 +23,7 @@ public class LightboxItemRepr
 	public String name;
 	public long size;
 	public URL lightboxContentsUrl;
+	public URL lightboxInstanceUrl;
 	public URL lightboxPublicUrl;
 
     // just to make JAXB happy
@@ -31,14 +32,24 @@ public class LightboxItemRepr
     	name = new String();
     	size = 0;
     	lightboxContentsUrl = new URL("http://example.com");
-    	lightboxPublicUrl = new URL("http://example.com");    	
-    };
+    	lightboxInstanceUrl = new URL("http://example.com");
+    	lightboxPublicUrl = new URL("http://example.com");
+    }
 
-	public LightboxItemRepr(String a_name, long a_size, URL a_lightboxContentsUrl, URL a_lightboxPublicUrl) 
+	public LightboxItemRepr(
+			String a_name,
+			long a_size,
+			URL a_lightboxInstanceUrl,
+			URL a_lightboxContentsUrl,
+			URL a_lightboxPublicUrl
+	)
 	{
 		name = a_name;
 		size = a_size;
+		lightboxInstanceUrl = a_lightboxInstanceUrl;
 		lightboxContentsUrl = a_lightboxContentsUrl;
 		lightboxPublicUrl = a_lightboxPublicUrl;
 	}
+
+
 }
