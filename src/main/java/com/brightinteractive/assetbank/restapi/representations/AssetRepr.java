d69cb341eb7f4b30dac4bd50e5b87626ae71f209
robotdevil <matt@bright-interactive.com>
2015-03-02 11:01:44 +0000

    Added parentIds to the AssetRepr.
    AB-2255

--- src/main/java/com/brightinteractive/assetbank/restapi/representations/AssetRepr.java
+++ src/main/java/com/brightinteractive/assetbank/restapi/representations/AssetRepr.java
@@ -3,6 +3,7 @@ package com.brightinteractive.assetbank.restapi.representations;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,6 +20,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class AssetRepr
 {
 	public boolean submitted;
+	public Collection<Long> parentIds;
 	
 	public URL url;
 	public URL contentUrl;
@@ -33,13 +35,14 @@ public class AssetRepr
     	attributes = new ArrayList<AttributeValueRepr>();
     }
 
-	public AssetRepr(URL url, URL contentUrl, URL displayUrl, List<AttributeValueRepr> attributes, boolean submitted)
+	public AssetRepr(URL url, URL contentUrl, URL displayUrl, List<AttributeValueRepr> attributes, boolean submitted, Collection<Long> parentIds)
 	{
 		this.submitted = submitted;
 		this.url = url;
 		this.contentUrl = contentUrl;
 		this.displayUrl = displayUrl;
 		this.attributes = Collections.unmodifiableList(attributes);
+		this.parentIds = parentIds;
 	}
 	
 	
