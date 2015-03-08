856e917203f00ac86fcf344ef999f3fc69ca7c44
Kevin Bennett <kevin@bright-interactive.co.uk>
2015-03-08 21:59:49 +0000

    Added Asset Entities Url to root resource

--- src/main/java/com/brightinteractive/assetbank/restapi/representations/RootRepr.java
+++ src/main/java/com/brightinteractive/assetbank/restapi/representations/RootRepr.java
@@ -73,6 +73,7 @@ public class RootRepr {
         public URL publishingActionsUrl;
         public URL displayAttributeGroupUrl;
         public URL tempChunkedFilesUrl;
+		public URL assetEntitiesUrl;
     }
 
     public String api_name;
@@ -101,7 +102,8 @@ public class RootRepr {
                     URL a_assetSearchUrl,
                     URL a_publishingActionsUrl,
                     URL a_displayAttributeGroupUrl,
-                    URL a_tempChunkedFilesUrl)
+                    URL a_tempChunkedFilesUrl,
+					URL a_assetEntitiesUrl)
     {
         api_name = API_NAME;
 
@@ -145,5 +147,6 @@ public class RootRepr {
         api_version_1_3.publishingActionsUrl = a_publishingActionsUrl;
         api_version_1_3.displayAttributeGroupUrl = a_displayAttributeGroupUrl;
         api_version_1_3.tempChunkedFilesUrl = a_tempChunkedFilesUrl;
+		api_version_1_3.assetEntitiesUrl = a_assetEntitiesUrl;
     }
 }
