0882b61e2b9d847ae935ca9907bba2aef4ca8333
robotdevil <matt@bright-interactive.com>
2018-06-05 16:28:46 +0100

    Rename fields as per pull request

--- src/main/java/com/brightinteractive/assetbank/restapi/representations/EmbeddedDataMappingRepr.java
+++ src/main/java/com/brightinteractive/assetbank/restapi/representations/EmbeddedDataMappingRepr.java
@@ -11,8 +11,8 @@ public class EmbeddedDataMappingRepr
 {
 	public URL url;
 	public String type = "";
-	public String name = "";
-	public String expression = "";
+	public String displayName = "";
+	public String tagName = "";
 	public String mappingDirection = "";
 	public URL attributeUrl;
 	public String attributeLabel = "";
@@ -23,16 +23,16 @@ public class EmbeddedDataMappingRepr
 
 	public EmbeddedDataMappingRepr(URL url,
 								   String type,
-								   String name,
-								   String expression,
+								   String displayName,
+								   String tagName,
 								   String mappingDirection,
 								   URL attributeUrl,
 								   String attributeLabel)
 	{
 		this.url = url;
 		this.type = type;
-		this.name = name;
-		this.expression = expression;
+		this.displayName = displayName;
+		this.tagName = tagName;
 		this.mappingDirection = mappingDirection;
 		this.attributeUrl = attributeUrl;
 		this.attributeLabel = attributeLabel;
