749a5da6e64554e3a8691007c13135a6637de650
Ana Lerma Giménez <ana@bright-interactive.co.uk>
2022-09-14 13:02:13 +0100

    ABC-6422 - Change XML root element name

--- src/main/java/com/brightinteractive/assetbank/restapi/representations/BatchContentUrlRepr.java
+++ src/main/java/com/brightinteractive/assetbank/restapi/representations/BatchContentUrlRepr.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name = "batchContentUrls")
+@XmlRootElement(name = "assetContentUrls")
 public class BatchContentUrlRepr {
   @XmlElementWrapper
   @XmlElement(name = "contentUrl")
